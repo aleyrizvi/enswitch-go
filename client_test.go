8001df08ae37274ea2f8f548ecec962240197af7
Asad Rizvi <aley.rizvi@icloud.com>
2023-10-23 19:40:00 +0300

    client_call test

--- client_test.go
+++ client_test.go
@@ -2,7 +2,9 @@ package enswitch
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 )
@@ -14,25 +16,85 @@ const (
 )
 
 func TestClientNewRequest(t *testing.T) {
-	c := Client{
-		httpClient: http.DefaultClient,
-		username:   testUsername,
-		password:   testPassword,
-		baseURL:    testBaseURL,
-		Customer:   nil,
+	type Test struct {
+		expectedError error
 	}
 
-	req, err := c.newRequest(context.Background(), "/test", url.Values{})
-	if err != nil {
-		t.Fatal("error occured")
+	cases := []Test{
+		{
+			expectedError: nil,
+		},
 	}
 
-	q, err := url.ParseQuery(req.URL.RawQuery)
-	if err != nil {
-		t.Fatal("error occured")
+	for _, c := range cases {
+		client := Client{
+			httpClient: http.DefaultClient,
+			username:   testUsername,
+			password:   testPassword,
+			baseURL:    testBaseURL,
+			Customer:   nil,
+		}
+
+		req, err := client.newRequest(context.Background(), "/test", url.Values{})
+		if err != c.expectedError {
+			t.Errorf("error occured while creating new request. Given %v, Got %v", c.expectedError, err)
+		}
+
+		q, err := url.ParseQuery(req.URL.RawQuery)
+		if err != nil {
+			t.Errorf("error occured while parsing query params. Given %v, Got %v", c.expectedError, err)
+		}
+
+		if q.Get("auth_username") != testUsername || q.Get("auth_password") != testPassword {
+			t.Error("error occured with query params")
+		}
+
+	}
+}
+
+func TestCall(t *testing.T) {
+	type Test struct {
+		body          string
+		expectedError error
+		expectedCode  uint
 	}
 
-	if q.Get("auth_username") != testUsername || q.Get("auth_password") != testPassword {
-		t.Error("error occured with query params")
+	cases := []Test{
+		{
+			body:          `{"responses": [{"key": "","code": "204","message": "OK"}]}`,
+			expectedError: nil,
+			expectedCode:  204,
+		},
+	}
+
+	for _, c := range cases {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, c.body)
+		}))
+		defer server.Close()
+
+		client := Client{
+			httpClient: http.DefaultClient,
+			username:   testUsername,
+			password:   testPassword,
+			baseURL:    server.URL,
+			Customer:   nil,
+		}
+
+		req, err := client.newRequest(context.Background(), "/", url.Values{})
+		if err != nil {
+			t.Errorf("error occured while creating new request. Given %v, Got %v", c.expectedError, err)
+		}
+
+		var response BalanceResponse
+
+		_, err = client.call(req, &response)
+		if err != c.expectedError {
+			t.Errorf("error occured calling http method. Given %v, Got %v", c.expectedError, err)
+		}
+
+		if response.Responses[0].Code != c.expectedCode {
+			t.Errorf("status code: given: %v, got: %v", c.expectedCode, response.Responses[0].Code)
+		}
 	}
 }
