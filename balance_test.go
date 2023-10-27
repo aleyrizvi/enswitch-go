8c0489fd05275814583f438aaa4ee0067c60a492
Asad Rizvi <aley.rizvi@icloud.com>
2023-10-27 14:30:33 +0300

    refactor balance test and pointer function

--- balance_test.go
+++ balance_test.go
@@ -2,11 +2,11 @@ package enswitch
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"reflect"
 	"testing"
 )
 
@@ -43,7 +43,7 @@ func TestBalance_Update(t *testing.T) {
 
 		res, err := balance.Update(context.Background(), c.input)
 
-		if err != c.errorExpected {
+		if errors.Is(err, c.errorExpected) {
 			t.Errorf("[balanceUpdate]: given %v, got %v", c.errorExpected, err)
 		}
 
@@ -54,387 +54,57 @@ func TestBalance_Update(t *testing.T) {
 }
 
 func TestCustomerBalanceQueryParams(t *testing.T) {
-	type Test struct {
-		b        BalanceParams
-		expected url.Values
-	}
-
 	transaction := TransactionTypeTopUp
 
-	cases := []Test{
+	cases := []struct {
+		params   BalanceParams
+		expected url.Values
+	}{
 		{
-			b: BalanceParams{
-				ID:     1,
-				Amount: 20,
+			params: BalanceParams{
+				ID:     123,
+				Amount: 45.67,
 			},
-			expected: map[string][]string{
-				"id":     {"1"},
-				"amount": {"20"},
+			expected: url.Values{
+				"id":     []string{"123"},
+				"amount": []string{"45.67"},
 			},
 		},
 		{
-			b: BalanceParams{
-				ID:          1,
-				Amount:      20,
-				Currency:    String("eur"),
-				Transaction: &transaction,
-				Description: String("test"),
+			params: BalanceParams{
+				ID:       456,
+				Amount:   78.90,
+				Currency: PointerTo("USD"),
 			},
-			expected: map[string][]string{
-				"id":          {"1"},
-				"amount":      {"20"},
-				"currency":    {"eur"},
-				"transaction": {"topup"},
-				"description": {"test"},
+			expected: url.Values{
+				"id":       []string{"456"},
+				"amount":   []string{"78.9"},
+				"currency": []string{"USD"},
+			},
+		},
+		{
+			params: BalanceParams{
+				ID:          789,
+				Amount:      12.34,
+				Currency:    PointerTo("EUR"),
+				Transaction: &transaction,
+				Description: PointerTo("Test description"),
+			},
+			expected: url.Values{
+				"id":          []string{"789"},
+				"amount":      []string{"12.34"},
+				"currency":    []string{"EUR"},
+				"transaction": []string{"topup"},
+				"description": []string{"Test description"},
 			},
 		},
 	}
 
 	for _, c := range cases {
-		qs := balanceQueryParam(c.b)
+		qs := balanceQueryParam(c.params)
 
-		if qs.Encode() != c.expected.Encode() {
+		if !urlValuesEqual(qs, c.expected) {
 			t.Errorf("[Balance] unable to create query param. Given %v, got %v", qs, c.expected)
 		}
-
-	}
-}
-
-func TestBalance_Update1(t *testing.T) {
-	type fields struct {
-		client *Client
-	}
-	type args struct {
-		ctx   context.Context
-		input BalanceParams
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *BalanceResponse
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &Balance{
-				client: tt.fields.client,
-			}
-			got, err := b.Update(tt.args.ctx, tt.args.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Update() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestBalance_Update2(t *testing.T) {
-	type fields struct {
-		client *Client
-	}
-	type args struct {
-		ctx   context.Context
-		input BalanceParams
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *BalanceResponse
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &Balance{
-				client: tt.fields.client,
-			}
-			got, err := b.Update(tt.args.ctx, tt.args.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Update() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestBool(t *testing.T) {
-	type args struct {
-		b bool
-	}
-	tests := []struct {
-		name string
-		args args
-		want *bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Bool(tt.args.b); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Bool() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestClient_call(t *testing.T) {
-	type fields struct {
-		httpClient *http.Client
-		username   string
-		password   string
-		baseURL    string
-		Customer   *Customers
-	}
-	type args struct {
-		req *http.Request
-		v   interface{}
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *http.Response
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Client{
-				httpClient: tt.fields.httpClient,
-				username:   tt.fields.username,
-				password:   tt.fields.password,
-				baseURL:    tt.fields.baseURL,
-				Customer:   tt.fields.Customer,
-			}
-			got, err := c.call(tt.args.req, tt.args.v)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("call() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("call() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestClient_newRequest(t *testing.T) {
-	type fields struct {
-		httpClient *http.Client
-		username   string
-		password   string
-		baseURL    string
-		Customer   *Customers
-	}
-	type args struct {
-		ctx context.Context
-		uri string
-		qs  url.Values
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *http.Request
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Client{
-				httpClient: tt.fields.httpClient,
-				username:   tt.fields.username,
-				password:   tt.fields.password,
-				baseURL:    tt.fields.baseURL,
-				Customer:   tt.fields.Customer,
-			}
-			got, err := c.newRequest(tt.args.ctx, tt.args.uri, tt.args.qs)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("newRequest() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("newRequest() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestClient_parseQueryParams(t *testing.T) {
-	type fields struct {
-		httpClient *http.Client
-		username   string
-		password   string
-		baseURL    string
-		Customer   *Customers
-	}
-	type args struct {
-		qs url.Values
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   url.Values
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Client{
-				httpClient: tt.fields.httpClient,
-				username:   tt.fields.username,
-				password:   tt.fields.password,
-				baseURL:    tt.fields.baseURL,
-				Customer:   tt.fields.Customer,
-			}
-			if got := c.parseQueryParams(tt.args.qs); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("parseQueryParams() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCustomers_Get(t *testing.T) {
-	type fields struct {
-		client  *Client
-		Balance *Balance
-	}
-	tests := []struct {
-		name   string
-		fields fields
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Customers{
-				client:  tt.fields.client,
-				Balance: tt.fields.Balance,
-			}
-			c.Get()
-		})
-	}
-}
-
-func TestCustomers_List(t *testing.T) {
-	type fields struct {
-		client  *Client
-		Balance *Balance
-	}
-	type args struct {
-		ctx   context.Context
-		input *CustomerListParams
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &Customers{
-				client:  tt.fields.client,
-				Balance: tt.fields.Balance,
-			}
-			c.List(tt.args.ctx, tt.args.input)
-		})
-	}
-}
-
-func TestNew(t *testing.T) {
-	type args struct {
-		username string
-		password string
-		baseURL  string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *Client
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.username, tt.args.password, tt.args.baseURL); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestString(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := String(tt.args.s); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_balanceQueryParam(t *testing.T) {
-	type args struct {
-		b BalanceParams
-	}
-	tests := []struct {
-		name string
-		args args
-		want url.Values
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := balanceQueryParam(tt.args.b); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("balanceQueryParam() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_getCustomerListOptions(t *testing.T) {
-	type args struct {
-		in0 *CustomerListParams
-	}
-	tests := []struct {
-		name string
-		args args
-		want url.Values
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getCustomerListOptions(tt.args.in0); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("getCustomerListOptions() = %v, want %v", got, tt.want)
-			}
-		})
 	}
 }
--- helpers.go
+++ helpers.go
@@ -1,9 +1,27 @@
 package enswitch
 
-func String(s string) *string {
-	return &s
+import "net/url"
+
+func PointerTo[T any](v T) *T {
+	return &v
 }
 
-func Bool(b bool) *bool {
-	return &b
+func urlValuesEqual(v1, v2 url.Values) bool {
+	if len(v1) != len(v2) {
+		return false
+	}
+
+	for key, values := range v1 {
+		if len(values) != len(v2[key]) {
+			return false
+		}
+
+		for i, val := range values {
+			if val != v2[key][i] {
+				return false
+			}
+		}
+	}
+
+	return true
 }
